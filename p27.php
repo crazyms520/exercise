b51f7c44b99a561ec5238606c570b5ddb147aaf6
crazyms <crazyms520@hotmail.com>
2015-11-23 23:22:28 +0800

    p27

---
+++ p27.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Try it youself p27</title>
+</head>
+<style type="text/css">
+.text1{
+  width: 100px;
+  color:rgba(243, 244, 245, 1);
+  background-color: rgba(107, 221, 79, 1);
+}
+.text2{
+  width: 100px;
+  color:rgba(243, 244, 245, 1);
+  background-color: red;
+}
+</style>
+<body>
+
+<?php
+  echo "<div class='text1'>";
+  echo "<font color='block'>1.</font></br>";
+  echo "&nbsp&nbsp&nbsp&nbsp*</br>";
+  echo "&nbsp&nbsp&nbsp&nbsp**</br>";
+  echo "*&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp***</br>";
+  echo "**&nbsp&nbsp&nbsp&nbsp&nbsp****</br>";
+  echo "**************</br>";
+  echo "**&nbsp&nbsp&nbsp&nbsp&nbsp****</br>";
+  echo "*&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp***</br>";
+  echo "&nbsp&nbsp&nbsp&nbsp**</br>";
+  echo "&nbsp&nbsp&nbsp&nbsp*</br>";
+  echo "</div>";
+  echo "</br></br>";
+  echo "<div class='text2'>";
+  echo "<font color='block'>2.</font></br>";
+  echo "&nbspA</br>";
+  echo "&nbsp&nbspI*</br>";
+  echo "&nbsp&nbspI**</br>";
+  echo "&nbsp&nbspI***</br>";
+  echo "&nbsp&nbspI****</br>";
+  echo "&nbsp&nbspI*****</br>";
+  echo "&nbsp&nbspI</br>";
+  echo "&nbsp&nbspI</br>";
+  echo "&nbsp&nbspI</br>";
+  echo "&nbsp&nbspI</br>";
+  echo "</div>";
+?>
+
+
+</body>
+</html>
